6306a91081940f161f954ca45e7dbe59b3b903af
Kaspar Ustav <54994820+Piiritaja@users.noreply.github.com>
2021-12-05 01:27:22 +0200

    Empty methods

--- src/main/java/ee/taltech/weathermap/api/WeatherApi.java
+++ src/main/java/ee/taltech/weathermap/api/WeatherApi.java
@@ -1,4 +1,11 @@
 package ee.taltech.weathermap.api;
 
+import ee.taltech.weathermap.model.CurrentWeatherData;
+
 public class WeatherApi {
+
+    public CurrentWeatherData getCurrentWeatherData(String cityName) {
+        //TODO
+        return null;
+    }
 }
--- src/main/java/ee/taltech/weathermap/model/CurrentWeatherData.java
+++ src/main/java/ee/taltech/weathermap/model/CurrentWeatherData.java
@@ -1,4 +1,14 @@
 package ee.taltech.weathermap.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
 public class CurrentWeatherData {
+    private String name;
 }
