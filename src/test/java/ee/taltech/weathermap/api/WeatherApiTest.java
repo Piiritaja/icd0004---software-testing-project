f3f6c82a4ce740faccc1b07c80ba88bbfb3cd8e2
Kaspar Ustav <54994820+Piiritaja@users.noreply.github.com>
2021-12-05 01:28:09 +0200

    add failing weatherApi test

--- src/test/java/ee/taltech/weathermap/api/WeatherApiTest.java
+++ src/test/java/ee/taltech/weathermap/api/WeatherApiTest.java
@@ -1,6 +1,8 @@
 package ee.taltech.weathermap.api;
 
+import ee.taltech.weathermap.model.CurrentWeatherData;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +10,15 @@ class WeatherApiTest {
 
     @BeforeEach
     void setUp() {
+    }
+
+    @Test
+    void shouldReturnCityNameInCurrentWeatherDataWhenCityNameIsCorrect() {
+        String cityName = "Keila";
+        WeatherApi weatherApi = new WeatherApi();
+
+        CurrentWeatherData weatherData = weatherApi.getCurrentWeatherData(cityName);
+
+        assertEquals(cityName, weatherData.getName());
     }
 }
