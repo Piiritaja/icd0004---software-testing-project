90b90f95113e011acc882c954a1144bbb80ec79d
Kaspar Ustav <54994820+Piiritaja@users.noreply.github.com>
2021-12-05 02:20:14 +0200

    WeatherApi implementation

--- src/main/java/ee/taltech/weathermap/api/WeatherApi.java
+++ src/main/java/ee/taltech/weathermap/api/WeatherApi.java
@@ -1,11 +1,34 @@
 package ee.taltech.weathermap.api;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
 import ee.taltech.weathermap.model.CurrentWeatherData;
+import ee.taltech.weathermap.store.KeyStore;
+import org.codehaus.jackson.jaxrs.JacksonJaxbJsonProvider;
+
+
+import static com.sun.jersey.api.json.JSONConfiguration.FEATURE_POJO_MAPPING;
 
 public class WeatherApi {
 
     public CurrentWeatherData getCurrentWeatherData(String cityName) {
-        //TODO
-        return null;
+        Client client = getConfiguredClient();
+
+        ClientResponse response = client.resource("https://api.openweathermap.org/data/2.5/weather")
+                .queryParam("appid", KeyStore.API_KEY)
+                .queryParam("q", cityName)
+                .queryParam("units", "metric")
+                .get(ClientResponse.class);
+
+        return response.getEntity(CurrentWeatherData.class);
+    }
+
+    private static Client getConfiguredClient() {
+        ClientConfig clientConfig = new DefaultClientConfig();
+        clientConfig.getClasses().add(JacksonJaxbJsonProvider.class);
+        clientConfig.getFeatures().put(FEATURE_POJO_MAPPING, true);
+        return Client.create(clientConfig);
     }
 }
