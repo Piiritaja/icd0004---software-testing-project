7cddb26865d4b573afa749939331fe909ad6d06c
Kaspar Ustav <54994820+Piiritaja@users.noreply.github.com>
2021-12-05 02:20:03 +0200

    WeatherApi implementation

--- src/main/java/ee/taltech/weathermap/model/CurrentWeatherData.java
+++ src/main/java/ee/taltech/weathermap/model/CurrentWeatherData.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CurrentWeatherData {
     private String name;
 }
--- src/main/java/ee/taltech/weathermap/store/KeyStore.java
+++ src/main/java/ee/taltech/weathermap/store/KeyStore.java
@@ -2,5 +2,5 @@ package ee.taltech.weathermap.store;
 
 public class KeyStore {
     //Your API key here
-    public static final String API_KEY = "";
+    public static final String API_KEY = "9a53e8b5c41e72d9a0422eb4a2cf7b11";
 }
