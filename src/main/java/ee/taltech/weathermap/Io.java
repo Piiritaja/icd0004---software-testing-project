664d4fdd2a21fdb474790c9b65f29d86c2a7ac74
Kaspar Ustav <54994820+Piiritaja@users.noreply.github.com>
2021-12-16 18:05:58 +0200

    io method return json instead

--- src/main/java/ee/taltech/weathermap/Io.java
+++ src/main/java/ee/taltech/weathermap/Io.java
@@ -1,11 +1,13 @@
 package ee.taltech.weathermap;
 
+import com.google.gson.Gson;
 import ee.taltech.weathermap.api.WeatherApi;
 import ee.taltech.weathermap.model.WeatherReport;
 
 public class Io {
 
-    public WeatherReport getWeatherReport(String cityName) {
-        return WeatherReport.from(WeatherApi.getWeatherData(cityName));
+    public String getWeatherReport(String cityName) {
+        Gson gson = new Gson();
+        return gson.toJson(WeatherReport.from(WeatherApi.getWeatherData(cityName)));
     }
 }
--- src/test/java/ee/taltech/weathermap/IoTest.java
+++ src/test/java/ee/taltech/weathermap/IoTest.java
@@ -1,5 +1,6 @@
 package ee.taltech.weathermap;
 
+import com.google.gson.Gson;
 import ee.taltech.weathermap.exception.InvalidCityNameException;
 import ee.taltech.weathermap.model.CurrentWeatherData;
 import ee.taltech.weathermap.model.MainWeatherData;
@@ -11,10 +12,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class IoTest {
     private Io io;
+    private Gson gson;
 
     @BeforeEach
     void setUp() {
         io = new Io();
+        gson = new Gson();
     }
 
     @Test
@@ -34,17 +37,17 @@ class IoTest {
 
     @Test
     public void whenCalledWithCorrectCityName_returnsWeatherReport() {
-        assertEquals(WeatherReport.class, io.getWeatherReport("Keila").getClass());
+        assertEquals(WeatherReport.class, gson.fromJson(io.getWeatherReport("Keila"), WeatherReport.class).getClass());
 
     }
 
     @Test
     public void shouldIncludeMainDetailsInWeatherReport_whenInputNameIsKeila() {
-        assertEquals(MainWeatherData.class, io.getWeatherReport("Keila").getMainDetails().getClass());
+        assertEquals(MainWeatherData.class, gson.fromJson(io.getWeatherReport("Keila"), WeatherReport.class).getMainDetails().getClass());
     }
 
     @Test
     public void shouldIncludeCurrentWeatherDataInWeatherData_whenInputNameIsKeila() {
-        assertEquals(CurrentWeatherData.class, io.getWeatherReport("Keila").getCurrentWeatherData().getClass());
+        assertEquals(CurrentWeatherData.class, gson.fromJson(io.getWeatherReport("Keila"), WeatherReport.class).getCurrentWeatherData().getClass());
     }
 }
