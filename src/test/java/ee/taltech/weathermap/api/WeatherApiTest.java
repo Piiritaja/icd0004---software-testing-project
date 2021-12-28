a7ef0e17ea0197d61d63926750ff4ea1608947f3
Kaspar Ustav <54994820+Piiritaja@users.noreply.github.com>
2021-12-29 00:20:54 +0200

    test if forecast response includes next three days

--- src/test/java/ee/taltech/weathermap/api/WeatherApiTest.java
+++ src/test/java/ee/taltech/weathermap/api/WeatherApiTest.java
@@ -1,10 +1,12 @@
 package ee.taltech.weathermap.api;
 
-import ee.taltech.weathermap.model.response.Coordinates;
 import ee.taltech.weathermap.model.response.WeatherDetailsResponse;
 import ee.taltech.weathermap.model.response.forecast.WeatherForecastResponse;
+import org.apache.commons.lang3.time.DateUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -67,6 +69,34 @@ class WeatherApiTest {
 
         WeatherForecastResponse weatherData = api.getWeatherForecast(cityName);
 
-        assertTrue(weatherData.getList().size() > 3);
+        assertTrue(weatherData.getList().size() > (3));
+    }
+
+    @Test
+    void shouldReturnNextThreeDayForecastInForecastResponseWhenCityNameIsKeila() {
+        String cityName = "Keila";
+
+        WeatherForecastResponse weatherData = api.getWeatherForecast(cityName);
+
+        Set<Date> dates = new HashSet<>();
+
+        // Generate next 3 days
+        for (int i = 1; i <= 3; i++) {
+            Date dt = new Date();
+            Calendar c = Calendar.getInstance();
+            c.setTime(dt);
+            c.add(Calendar.DATE, i);
+            dates.add(DateUtils.truncate(c.getTime(), Calendar.DAY_OF_MONTH));
+        }
+
+        // Response contains next 3 days?
+        for (var item : weatherData.getList()) {
+            Date formDate = DateUtils.truncate(item.getDate(), Calendar.DAY_OF_MONTH);
+            dates.remove(formDate);
+            if (dates.isEmpty()) {
+                assertTrue(true);
+            }
+        }
+        assertTrue(dates.isEmpty());
     }
 }
