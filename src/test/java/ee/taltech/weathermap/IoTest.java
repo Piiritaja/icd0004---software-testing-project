3ddf748d0e737fbed208fe2bfe52bc9cc3ba2ac0
Kaspar Ustav <54994820+Piiritaja@users.noreply.github.com>
2021-12-29 01:54:44 +0200

    date tests

--- src/test/java/ee/taltech/weathermap/IoTest.java
+++ src/test/java/ee/taltech/weathermap/IoTest.java
@@ -2,12 +2,17 @@ package ee.taltech.weathermap;
 
 import ee.taltech.weathermap.api.WeatherApi;
 import ee.taltech.weathermap.exception.InvalidCityNameException;
-import ee.taltech.weathermap.model.CurrentWeatherData;
-import ee.taltech.weathermap.model.ForecastReport;
-import ee.taltech.weathermap.model.MainWeatherData;
-import ee.taltech.weathermap.model.WeatherReport;
+import ee.taltech.weathermap.model.*;
+import ee.taltech.weathermap.model.response.Weather;
+import org.apache.commons.lang3.time.DateUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -41,6 +46,17 @@ class IoTest {
     }
 
     @Test
+    public void shouldIncludeCurrentDateInCurrentWeatherData_whenCityNameIsKeila() {
+        WeatherReport weatherReport = io.getWeatherReport("Keila");
+        Date dt = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(dt);
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        assertEquals(dateFormat.format(dt), weatherReport.getCurrentWeatherData().getWeatherData().getDate());
+    }
+
+
+    @Test
     public void shouldIncludeMainDetailsInWeatherReport_whenInputNameIsKeila() {
         assertNotNull(io.getWeatherReport("Keila").getMainDetails());
         assertEquals(MainWeatherData.class, io.getWeatherReport("Keila").getMainDetails().getClass());
@@ -53,8 +69,23 @@ class IoTest {
     }
 
     @Test
-    public void shouldIncludeForecastRepostInWeatherData_whenInputNameIsKeila(){
+    public void shouldIncludeForecastRepostInForecastReport_whenInputNameIsKeila() {
         assertNotNull(io.getWeatherReport("Keila").getForecastReport());
         assertEquals(ForecastReport.class, io.getWeatherReport("Keila").getForecastReport().getClass());
+    }
+
+    @Test
+    public void shouldIncludeThreeDayForecastInForecastReport_whenInputNameIsKeila() {
+        List<WeatherData> weather = io.getWeatherReport("Keila").getForecastReport().getWeatherData();
+        assertTrue(weather.size() >= 3);
+        for (int i = 0; i < 3; i++) {
+            WeatherData weatherData = weather.get(i);
+            Date dt = new Date();
+            Calendar c = Calendar.getInstance();
+            c.setTime(dt);
+            c.add(Calendar.DATE, i + 1);
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            assertEquals(dateFormat.format(dt), weatherData.getDate());
+        }
     }
 }
