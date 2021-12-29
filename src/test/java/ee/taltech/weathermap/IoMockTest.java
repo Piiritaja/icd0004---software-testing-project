cd1d8c513c496dde1e66f31af0618efcb8b51908
Kaspar Ustav <54994820+Piiritaja@users.noreply.github.com>
2021-12-29 03:16:13 +0200

    Mock tests for weather forecast

--- src/test/java/ee/taltech/weathermap/IoMockTest.java
+++ src/test/java/ee/taltech/weathermap/IoMockTest.java
@@ -3,10 +3,9 @@ package ee.taltech.weathermap;
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 import ee.taltech.weathermap.api.WeatherApi;
-import ee.taltech.weathermap.model.CurrentWeatherData;
-import ee.taltech.weathermap.model.MainWeatherData;
-import ee.taltech.weathermap.model.WeatherReport;
+import ee.taltech.weathermap.model.*;
 import ee.taltech.weathermap.model.response.*;
+import ee.taltech.weathermap.model.response.forecast.WeatherForecastResponse;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,6 +18,7 @@ import java.io.FileReader;
 import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,6 +38,13 @@ public class IoMockTest {
         String mockFile = mockCity.toLowerCase(Locale.ROOT) + "Mock.json";
         JsonReader reader = new JsonReader(new FileReader("mockData/" + mockFile));
         return new Gson().fromJson(reader, WeatherDetailsResponse.class);
+    }
+
+    @SneakyThrows
+    private WeatherForecastResponse loadForecastMock(String mockCity) throws FileNotFoundException {
+        String mockFile = mockCity.toLowerCase(Locale.ROOT) + "ForecastMock.json";
+        JsonReader reader = new JsonReader(new FileReader("mockData/" + mockFile));
+        return new Gson().fromJson(reader, WeatherForecastResponse.class);
     }
 
     @Test
@@ -68,21 +75,63 @@ public class IoMockTest {
 
     @Test
     @SneakyThrows
-    public void shouldHaveCoordinatesInMainDetails_whenCityNameKeila(){
+    public void shouldHaveCoordinatesInMainDetails_whenCityNameKeila() {
         String cityName = "Keila";
         WeatherDetailsResponse weatherDetails = loadMock(cityName);
         when(weatherApi.getWeatherData(cityName)).thenReturn(weatherDetails);
         WeatherReport weatherReport = io.getWeatherReport(cityName);
-        assertEquals("59.30,24.41",weatherReport.getMainDetails().getCoordinates());
+        assertEquals("59.30,24.41", weatherReport.getMainDetails().getCoordinates());
     }
 
     @Test
     @SneakyThrows
-    public void shouldHaveTemperatureUnitInMainDetails_whenCityNameKeila(){
+    public void shouldHaveTemperatureUnitInMainDetails_whenCityNameKeila() {
         String cityName = "Keila";
         WeatherDetailsResponse weatherDetails = loadMock(cityName);
         when(weatherApi.getWeatherData(cityName)).thenReturn(weatherDetails);
         WeatherReport weatherReport = io.getWeatherReport(cityName);
         assertEquals("celsius", weatherReport.getMainDetails().getTemperatureUnit());
     }
+
+    @Test
+    @SneakyThrows
+    public void shouldHaveForecastInWeatherReport_whenCityNameKeila() {
+        String cityName = "Keila";
+        WeatherDetailsResponse weatherDetails = loadMock(cityName);
+        WeatherForecastResponse weatherForecastResponse = loadForecastMock(cityName);
+        when(weatherApi.getWeatherData(cityName)).thenReturn(weatherDetails);
+        when(weatherApi.getWeatherForecast(cityName)).thenReturn(weatherForecastResponse);
+        WeatherReport weatherReport = io.getWeatherReport(cityName);
+        assertNotNull(weatherReport.getForecastReport());
+        assertEquals(ForecastReport.class, weatherReport.getForecastReport().getClass());
+    }
+
+    @Test
+    @SneakyThrows
+    public void shouldHaveThreeDaysInForecast_whenCityNameKeila() {
+        String cityName = "Keila";
+        WeatherDetailsResponse weatherDetails = loadMock(cityName);
+        WeatherForecastResponse weatherForecastResponse = loadForecastMock(cityName);
+        when(weatherApi.getWeatherData(cityName)).thenReturn(weatherDetails);
+        when(weatherApi.getWeatherForecast(cityName)).thenReturn(weatherForecastResponse);
+        WeatherReport weatherReport = io.getWeatherReport(cityName);
+        assertEquals(3, weatherReport.getForecastReport().getWeatherData().size());
+    }
+
+    @Test
+    @SneakyThrows
+    public void shouldHaveForecastDatesInAscendingOrder_whenCityNameKeila() {
+        String cityName = "Keila";
+        WeatherDetailsResponse weatherDetails = loadMock(cityName);
+        WeatherForecastResponse weatherForecastResponse = loadForecastMock(cityName);
+        when(weatherApi.getWeatherData(cityName)).thenReturn(weatherDetails);
+        when(weatherApi.getWeatherForecast(cityName)).thenReturn(weatherForecastResponse);
+        WeatherReport weatherReport = io.getWeatherReport(cityName);
+        assertEquals("2021-12-30", weatherReport.getForecastReport().getWeatherData().get(0).getDate());
+        assertEquals("2021-12-31", weatherReport.getForecastReport().getWeatherData().get(1).getDate());
+        assertEquals("2022-01-01", weatherReport.getForecastReport().getWeatherData().get(2).getDate());
+
+    }
+
+
 }
