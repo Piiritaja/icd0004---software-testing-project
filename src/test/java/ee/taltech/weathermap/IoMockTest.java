ce42546171fe16606b50696765d071ba2835f8f5
Kaspar Ustav <54994820+Piiritaja@users.noreply.github.com>
2021-12-30 16:01:50 +0200

    mock localDate

--- src/test/java/ee/taltech/weathermap/IoMockTest.java
+++ src/test/java/ee/taltech/weathermap/IoMockTest.java
@@ -11,10 +11,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.time.LocalDate;
 import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -123,14 +126,18 @@ public class IoMockTest {
     public void shouldHaveForecastDatesInAscendingOrder_whenCityNameKeila() {
         String cityName = "Keila";
         WeatherDetailsResponse weatherDetails = loadMock(cityName);
-        WeatherForecastResponse weatherForecastResponse = loadForecastMock(cityName);
-        when(weatherApi.getWeatherData(cityName)).thenReturn(weatherDetails);
-        when(weatherApi.getWeatherForecast(cityName)).thenReturn(weatherForecastResponse);
-        WeatherReport weatherReport = io.getWeatherReport(cityName);
-        assertEquals("2021-12-30", weatherReport.getForecastReport().getWeatherData().get(0).getDate());
-        assertEquals("2021-12-31", weatherReport.getForecastReport().getWeatherData().get(1).getDate());
-        assertEquals("2022-01-01", weatherReport.getForecastReport().getWeatherData().get(2).getDate());
 
+        LocalDate mockLocalDate = LocalDate.of(2021, 12, 29);
+        try (MockedStatic<LocalDate> topDateTimeUtilMock = Mockito.mockStatic(LocalDate.class)) {
+            topDateTimeUtilMock.when(LocalDate::now).thenReturn(mockLocalDate);
+            WeatherForecastResponse weatherForecastResponse = loadForecastMock(cityName);
+            when(weatherApi.getWeatherData(cityName)).thenReturn(weatherDetails);
+            when(weatherApi.getWeatherForecast(cityName)).thenReturn(weatherForecastResponse);
+            WeatherReport weatherReport = io.getWeatherReport(cityName);
+            assertEquals("2021-12-30", weatherReport.getForecastReport().getWeatherData().get(0).getDate());
+            assertEquals("2021-12-31", weatherReport.getForecastReport().getWeatherData().get(1).getDate());
+            assertEquals("2022-01-01", weatherReport.getForecastReport().getWeatherData().get(2).getDate());
+        }
     }
 
 
