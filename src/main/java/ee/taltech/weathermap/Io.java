db9be6097e8a94d3657600bd5c05c3204478ee00
Kaspar Ustav <54994820+Piiritaja@users.noreply.github.com>
2021-12-30 16:34:58 +0200

    implement writing report to file

--- src/main/java/ee/taltech/weathermap/Io.java
+++ src/main/java/ee/taltech/weathermap/Io.java
@@ -1,6 +1,8 @@
 package ee.taltech.weathermap;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonWriter;
 import ee.taltech.weathermap.api.WeatherApi;
 import ee.taltech.weathermap.exception.FileTypeNotSupportedException;
 import ee.taltech.weathermap.model.ForecastReport;
@@ -10,14 +12,13 @@ import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Slf4j
@@ -27,14 +28,14 @@ public class Io {
 
     private WeatherApi api;
 
+    private String outputPath;
+
     public WeatherReport getWeatherReport(String cityName) {
-        Gson gson = new Gson();
         WeatherReport weatherReport = WeatherReport.from(api.getWeatherData(cityName));
         ForecastReport forecastReport = getWeatherForecastReport(cityName);
         if (forecastReport != null) {
             weatherReport.setForecastReport(forecastReport);
         }
-        System.out.println(gson.toJson(weatherReport));
         return weatherReport;
     }
 
@@ -49,19 +50,39 @@ public class Io {
         List<String> cityNames = new ArrayList<>();
         log.info("Reading input file");
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            String cityName = br.readLine();
-            log.debug("Read cityName " + cityName);
-            if (!cityName.isBlank()){
-                cityNames.add(cityName);
-                log.debug("Added " + cityName + " to city names");
+            String cityName;
+            while ((cityName = br.readLine()) != null && !cityName.isBlank()) {
+                log.debug("Read cityName " + cityName);
+                if (!cityName.isBlank()) {
+                    cityNames.add(cityName);
+                    log.debug("Added " + cityName + " to city names");
 
+                }
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             log.error("Could not read file");
             throw new FileNotFoundException();
         }
 
+        List<WeatherReport> reports = cityNames.stream().map(this::getWeatherReport).collect(Collectors.toList());
+
+        for (WeatherReport report : reports) {
+            this.writeReportToFile(report);
+        }
+
         return new WeatherReport();
+    }
+
+    private void writeReportToFile(WeatherReport weatherReport) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String outputFileName = outputPath + weatherReport.getMainDetails().getCity().toLowerCase(Locale.ROOT) + "_report.json";
+        try (FileWriter fileWriter = new FileWriter(outputFileName)) {
+            gson.toJson(weatherReport, fileWriter);
+            log.info("Report in file " + outputFileName);
+        } catch (IOException ignore) {
+
+        }
+
     }
 
     public ForecastReport getWeatherForecastReport(String cityName) {
