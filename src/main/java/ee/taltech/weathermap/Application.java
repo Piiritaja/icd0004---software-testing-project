7a65eae303779a8407b4aed368ce3548ff4b2bc0
Kaspar Ustav <54994820+Piiritaja@users.noreply.github.com>
2022-01-02 20:42:41 +0200

    update running application

--- src/main/java/ee/taltech/weathermap/Application.java
+++ src/main/java/ee/taltech/weathermap/Application.java
@@ -7,10 +7,13 @@ import lombok.extern.slf4j.Slf4j;
 public class Application {
 
     public static void main(String[] args) {
-        log.info("init");
+        if (args.length < 1) {
+            log.info("Need to pass a .txt file as argument");
+            return;
+        }
         String file_location = args[0];
         WeatherApi api = new WeatherApi();
-        Io io = new Io(api, ".");
+        Io io = new Io(api, "");
         io.generateWeatherReport(file_location);
     }
 }
