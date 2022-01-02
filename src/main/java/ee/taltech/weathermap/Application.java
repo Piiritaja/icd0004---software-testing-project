bbc21a21e4a2463d548abbdce1d1ea0b922bd7fc
Kaspar Ustav <54994820+Piiritaja@users.noreply.github.com>
2022-01-02 02:48:21 +0200

    build jar and include dependencies with gradle

--- src/main/java/ee/taltech/weathermap/Application.java
+++ src/main/java/ee/taltech/weathermap/Application.java
@@ -1,10 +1,16 @@
 package ee.taltech.weathermap;
 
+import ee.taltech.weathermap.api.WeatherApi;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Application {
 
     public static void main(String[] args) {
+        log.info("init");
         String file_location = args[0];
-
-        System.out.println(file_location);
+        WeatherApi api = new WeatherApi();
+        Io io = new Io(api, ".");
+        io.generateWeatherReport(file_location);
     }
 }
